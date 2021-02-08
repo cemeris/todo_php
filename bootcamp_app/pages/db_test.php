a4104821279cee1c7de9ae7ab3e9bee491cc916e
cemeris <4vitalik4@gmail.com>
2021-02-08 19:56:10 +0200

    moved cars db connection to object oriented

--- bootcamp_app/pages/db_test.php
+++ bootcamp_app/pages/db_test.php
@@ -1,45 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "test";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+include "../bootcamp_app/classes/Cars.php";
 
-getData($conn);
-//setData($conn);
-
-function getData($conn) {
-  $sql = "SELECT id, model, color FROM cars";
-  $result = $conn->query($sql);
-  
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Name: " . $row["model"]. " " . $row["color"]. "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
-}
-
-function setData($conn) {
-  $sql = "INSERT INTO cars (model, color) VALUES ('volkswagen', 'green')";
-  $result = $conn->query($sql);
-
-  if ($result === true) {
-    echo "ieraksts pievienots";
-  }
-  else {
-    echo "neizdevās";
-  }
-}
-
-
-$conn->close();
-
-?>
+$cars = new Cars();
+$cars->getData();
